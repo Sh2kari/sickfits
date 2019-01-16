90750af86adff614dcb7c91469e949f03fb7edb5
Sh2kari <buba_sev@bk.ru>
2019-01-16 14:48:02 +0300

    add removeFromCart

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -235,6 +235,27 @@ const Mutations = {
       },
       info
     );
+  },
+  async removeFromCart(parent, args, ctx, info) {
+    const cartItem = await ctx.db.query.cartItem(
+      {
+        where: { id: args.id }
+      },
+      `{ id, user { id } }`
+    );
+
+    if (!cartItem) throw new Error('No CartItem found');
+
+    if (cartItem.user.id !== ctx.request.userId) {
+      throw new Error('Cheetin huh');
+    }
+
+    return ctx.db.mutation.deleteCartItem(
+      {
+        where: { id: args.id }
+      },
+      info
+    );
   }
 };
 
